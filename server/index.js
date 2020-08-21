78299bdecc6755239a7d05720098ce6d5af24d4e
ABHISHEK AMRUTE <abhishekamrutelfs@gmail.com>
2020-08-21 08:05:45 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path  =require('path')
+const hbs = require('hbs')
+const http = require('http')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+
+const io = socketio(server)
+
+app.use(express.json())
+const pathdirectories = path.join(__dirname , "../public")
+app.use(express.static(pathdirectories))
+app.set('view engine', 'hbs')
+
+const temppath = path.join(__dirname , "../templates/views")
+app.set('views',temppath)
+
+app.get('',(req,res)=>
+{
+    res.render('index')
+})
+
+
+
+////socket part
+
+io.on('connection',(socket)=>{
+    console.log('New Connection')
+})
+
+
+server.listen(3000,()=>{
+    console.log("listning on 3000")
+})
